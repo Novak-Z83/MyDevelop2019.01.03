b7624a9bf4ec92ed81e32922874fce9b047f1d78
Zoltán Novák <novak.z83@live.com>
2019-04-22 15:54:10 +0200

    New stuff

---
+++ CardGame/Controllers/AppController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(object model)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+    }
+}
